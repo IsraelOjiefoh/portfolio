f61bb422fde329fff99e21ed3bfe2522128339e4
Israel Ojiefoh <israelmayowa580@gmail.com>
2023-09-15 03:59:21 +0100

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import "./App.css";
 import Home from "./components/home";
-import NavBar from "./components/navbar";
 import Footer from "./components/footer";
 function App() {
   return (
     <>
       <div className="App">
-        <div className="navbar">
-          <NavBar />
-        </div>
         <div className="home">
           <Home />
         </div>
