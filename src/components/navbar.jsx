b32e2c2d704d26d879e298b5f152dd405dc45758
Israel Ojiefoh <israelmayowa580@gmail.com>
2023-09-15 05:10:41 +0100

    modified

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -8,8 +8,9 @@ import {
 } from "react-icons/fa";
 
 function MyNavbar() {
+  // eslint-disable-next-line no-unused-vars
   const [isClicked, setIsClicked] = useState("Home");
-  
+
   return (
     <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
       <Container>
@@ -17,30 +18,17 @@ function MyNavbar() {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ml-auto">
-            <Nav.Link
-              onClick={() => setIsClicked("Home")}
-              href="/"
-              className={isClicked === "Home" ? "is-clicked" : ""}
-            >
+            <Nav.Link onClick={() => setIsClicked("Home")} href="/">
               <FaHome /> Home
             </Nav.Link>
-            <Nav.Link
-              onClick={() => setIsClicked("About")}
-              href="/about"
-              className={isClicked === "About" ? "is-clicked" : ""}
-            >
+            <Nav.Link onClick={() => setIsClicked("About")} href="/about">
               <FaInfoCircle /> About
             </Nav.Link>
-            <Nav.Link
-              href="/projects"
-              className={isClicked === "Projects" ? "is-clicked" : ""}
-              onClick={() => setIsClicked("Projects")}
-            >
+            <Nav.Link href="/projects" onClick={() => setIsClicked("Projects")}>
               <FaFolderOpen /> Projects
             </Nav.Link>
             <Nav.Link
               href="/certificates"
-              className={isClicked === "Certificates" ? "is-clicked" : ""}
               onClick={() => setIsClicked("Certificates")}
             >
               <FaCertificate /> Certificates
