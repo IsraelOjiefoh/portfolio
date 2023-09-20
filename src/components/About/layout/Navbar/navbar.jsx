e373110ca50dad64c3b89923bf9cb275a43fd1eb
Israel Ojiefoh <israelmayowa580@gmail.com>
2023-09-20 23:56:58 +0100

    modified

--- src/components/About/layout/Navbar/navbar.jsx
+++ src/components/About/layout/Navbar/navbar.jsx
@@ -1,119 +1,85 @@
-import { useState } from "react";
-// import { Row, Container, NavLink } from "react-bootstrap";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./navbar.css";
 
-function YourNavbar() {
+const YourNavbar = () => {
   const [isClicked, setIsClicked] = useState("Home");
+
+  useEffect(() => {
+    // Add event listener to close mobile menu when clicking outside
+    const handleClickOutside = (event) => {
+      const checkbox = document.getElementById("check");
+      if (checkbox.checked && !event.target.closest(".nav-wrapper")) {
+        checkbox.checked = false;
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return (
     <div>
-      {/* <Container > */}
-      <nav className="fixed-top ">
-        <input type="checkbox" id="check"></input>
-        <label htmlFor="check" className="checkbtn">
-          <i className="fas fa-bars"></i>
-        </label>
-        <label className="logo">Israel Ojiefoh</label>
-
-        <ul>
-          <li className="">
-            <Link
-              to="/"
-              onClick={() => setIsClicked("Home", console.log("Home "))}
-              className={
-                isClicked === "Home" ? "nav-link-active" : "not-clicked"
-              }
-            >
-              Home
-            </Link>
-          </li>
-          <li className="">
-            <Link
-              to="/about"
-              onClick={() => setIsClicked("About")}
-              className={
-                isClicked === "About" ? "nav-link-active" : "not-clicked"
-              }
-            >
-              about
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/projects"
-              onClick={() => setIsClicked("projects")}
-              className={
-                isClicked === "Projects" ? "nav-link-active" : "not-clicked"
-              }
-            >
-              Projects
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/services"
-              onClick={() => setIsClicked("services")}
-              className={
-                isClicked === "services" ? "nav-link-active" : "not-clicked"
-              }
-            >
-              Services
-            </Link>
-          </li>
-        </ul>
+      <nav className="fixed-top">
+        <div className="nav-wrapper">
+          <input type="checkbox" id="check"></input>
+          <label htmlFor="check" className="checkbtn">
+            <i className="fas fa-bars"></i>
+          </label>
+          <label className="logo">Israel Ojiefoh</label>
+          <ul>
+            <li className="">
+              <Link
+                to="/"
+                onClick={() => setIsClicked("Home")}
+                className={
+                  isClicked === "Home" ? "nav-link-active" : "not-clicked"
+                }
+              >
+                Home
+              </Link>
+            </li>
+            <li className="">
+              <Link
+                to="/about"
+                onClick={() => setIsClicked("About")}
+                className={
+                  isClicked === "About" ? "nav-link-active" : "not-clicked"
+                }
+              >
+                About
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/projects"
+                onClick={() => setIsClicked("Projects")}
+                className={
+                  isClicked === "Projects" ? "nav-link-active" : "not-clicked"
+                }
+              >
+                Projects
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/services"
+                onClick={() => setIsClicked("Services")}
+                className={
+                  isClicked === "Services" ? "nav-link-active" : "not-clicked"
+                }
+              >
+                Services
+              </Link>
+            </li>
+          </ul>
+        </div>
       </nav>
-      {/* <Row className="navigation-row">
-          <NavLink className="text-decoration-none fs-5 d-flex justify-content-around">
-            <Link
-              className={
-                isClicked === "Home" ? "nav-link-active" : "not-clicked"
-              }
-              to="/"
-              onClick={() => {
-                setIsClicked("Home");
-              }}
-            >
-              Home
-            </Link>
-            <Link
-              to="/about"
-              className={
-                isClicked === "About" ? "nav-link-active" : "not-clicked"
-              }
-              onClick={() => {
-                setIsClicked("About");
-              }}
-            >
-              About
-            </Link>
-            <Link
-              to="/projects"
-              className={
-                isClicked === "Projects" ? "nav-link-active" : "not-clicked"
-              }
-              onClick={() => {
-                setIsClicked("Projects");
-              }}
-            >
-              Projects
-            </Link>
-            <Link
-              to="/services"
-              className={
-                isClicked === "Services" ? "nav-link-active" : "not-clicked"
-              }
-              onClick={() => {
-                setIsClicked("Services");
-              }}
-            >
-              Services
-            </Link>
-          </NavLink>
-        </Row> */}
-      {/* </Container> */}
     </div>
   );
-}
+};
 
 export default YourNavbar;
