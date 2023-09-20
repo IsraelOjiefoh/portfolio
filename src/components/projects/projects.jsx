39360846ec17b7fc1e312177b3200ea06b2a5287
Israel Ojiefoh <israelmayowa580@gmail.com>
2023-09-20 02:18:09 +0100

    mdified

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -1,32 +1,31 @@
 import portfolioImg from "./portfolio.jpg";
+import MemeImg from "./meme.jpg";
 import "./projects.css";
 const Projects = () => {
   const project = [
     {
       id: 1,
-      title: "Portfolio Website",
-      ImgUrl: portfolioImg,
-      technologyUsed: "CSS, React, BootStrap, Express",
-      Features:
-        "Responsive design, interactive elements, portfolio project showcase, and a contact form.",
+      imgUrl: portfolioImg,
+      title: "Personal Portfolio Website",
+
       liveDemo: "https://israelojiefoh.netlify.app/",
       gitHubRepo: "http://www.github.com/IsraelOjiefoh/portfolio",
+    },
+    {
+      id: 2,
+      imgUrl: MemeImg,
+      title: "Meme Viewer",
+      liveDemo: "https://meme2.netlify.app/",
+      gitHubRepo: "http://www.github.com/IsraelOjiefoh/meme",
     },
   ];
   return (
     <div className="projects">
       {project.map((item) => (
         <div className="item" key={item.id}>
-          <img src={item.ImgUrl} alt="image" className="card-img-top w-75" />
+          <img src={item.imgUrl} alt="image" className="w-75" />
           <div className=" pb-3 fs-4">{item.title}</div>
-          <div className="pb-3">
-            <strong>Key Features: </strong>
-            {item.Features}
-          </div>
-          <div className="pb-3">
-            <strong>Technology Used: </strong>
-            {item.technologyUsed}
-          </div>
+
           <a href={item.gitHubRepo} target="_blank ">
             <button className="btn btn-light mb-4 me-4">GitHub</button>
             <a href={item.liveDemo} target="_blank ">
