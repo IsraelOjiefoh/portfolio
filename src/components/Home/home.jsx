4e26a561e43bb1a37d4da50f33cf872306bac312
Israel Ojiefoh <israelmayowa580@gmail.com>
2023-10-03 14:28:23 +0100

    modified

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -1,18 +1,22 @@
+/* eslint-disable react/jsx-no-target-blank */
 import { Container, Row, Col } from "react-bootstrap";
 import homeLogo from "./home-main.svg";
 import "./homes.css";
 import Home2 from "./home2";
 import Type from "../../Type";
+import myCV from "./My_Resume.pdf";
+
+
 function Home() {
+
   return (
     <section>
-      <Container fluid  id="home">
+      <Container fluid id="home">
         <Container className="home-content">
           <Row>
             <Col md={7} className="home-header">
               <h1 style={{ paddingBottom: 15 }} className="heading">
                 Hi There!{" "}
-                
               </h1>
 
               <h1 className="heading-name">
@@ -34,6 +38,9 @@ function Home() {
               />
             </Col>
           </Row>
+          <a href={myCV} target="_blank" >
+            <button className="btn btn-success">DownLoad CV</button>
+          </a>
         </Container>
       </Container>
       <hr />
